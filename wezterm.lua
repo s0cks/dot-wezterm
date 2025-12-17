e33038e9b37d4ae77be62f23ec2c88a65789cf3d
tazz <tazz@tazz.cloud>
2025-12-17 11:44:40 -0500

    refactor: 🔨 code cleanup

--- wezterm.lua
+++ wezterm.lua
@@ -9,23 +9,6 @@ config.font_size = 14
 
 config.color_scheme = 'flexoki-dark'
 config.default_prog = { '/opt/homebrew/bin/zellij', 'attach', 'main', '--create' }
-config.font = wt.font({
-  family = 'Monaspace Argon Var',
-  style = 'Normal',
-  harfbuzz_features = {
-    '+liga',
-    '+ital',
-    '+ss01',
-    '+ss02',
-    '+ss03',
-    '+ss04',
-    '+ss05',
-    '+ss06',
-    '+ss07',
-    '+ss08',
-    '+ss09',
-    '+ss10',
-  },
-})
+config.font = require('font')
 config.window_background_opacity = 0.7
 return config
