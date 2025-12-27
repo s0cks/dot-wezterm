5f2349e6f4418966e193fd602763d25975f53885
tazz <tazz@tazz.cloud>
2025-12-27 14:08:23 -0500

    fix: 🐛 fix issue w/ wezterm crashing

--- wezterm.lua
+++ wezterm.lua
@@ -2,13 +2,9 @@
 ---@license MIT
 
 local wt = require('wezterm') ---@type Wezterm
-print('LUA_PATH: ' .. os.getenv('LUA_PATH'))
 
-local runtime_paths = {
-  os.getenv('LUA_PATH'),
-}
+local runtime_paths = {}
 package.path = package.path .. ';' .. (table.concat(runtime_paths, ';'))
-
 print('package.path (after): ' .. package.path)
 
 -- wt.on('update-status', require('update-status'))
