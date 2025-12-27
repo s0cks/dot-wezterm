f47f4cf945e44243d19557079477fb3ea1b556a4
tazz <tazz@tazz.cloud>
2025-12-27 14:06:19 -0500

    feat: ✨ add some debug stuff

--- wezterm.lua
+++ wezterm.lua
@@ -2,22 +2,14 @@
 ---@license MIT
 
 local wt = require('wezterm') ---@type Wezterm
-print(package.path)
+print('LUA_PATH: ' .. os.getenv('LUA_PATH'))
 
 local runtime_paths = {
-  wt.home_dir .. '/.local/share/mise/installs/lua/5.2/luarocks/share/lua/5.2/?.lua',
-  wt.home_dir .. '/.local/share/mise/installs/lua/5.2/luarocks/share/lua/5.2/?/init.lua',
-  wt.home_dir .. '/.local/share/mise/downloads/lua/5.2/lua-5.4.8/luarocks-3.12.2/src/?.lua',
-  '/usr/local/share/lua/5.2/?.lua',
-  '/usr/local/share/lua/5.2/?/init.lua',
-  '/usr/local/lib/lua/5.2/?.lua',
-  '/usr/local/lib/lua/5.2/?/init.lua',
-  wt.home_dir .. '/.luarocks/share/lua/5.2/?.lua',
-  wt.home_dir .. '/.luarocks/share/lua/5.2/?/init.lua',
-  './?.lua',
-  './?/init.lua',
+  os.getenv('LUA_PATH'),
 }
 package.path = package.path .. ';' .. (table.concat(runtime_paths, ';'))
+
+print('package.path (after): ' .. package.path)
 
 -- wt.on('update-status', require('update-status'))
 
