5929be6ffc9b6d3d8aa006de9bb5d41362af08c9
tazz <tazz@tazz.cloud>
2025-12-27 14:10:36 -0500

    fix: 🐛 lets try this

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,12 @@
 ---@license MIT
 
 local wt = require('wezterm') ---@type Wezterm
+local home_dir = os.getenv('HOME')
+print('home: ' .. home_dir)
 
-local runtime_paths = {}
+local runtime_paths = {
+  '/Users/tazz/.local/share/mise/installs/lua/5.4.8/luarocks/share/lua/5.4/?.lua;/Users/tazz/.local/share/mise/installs/lua/5.4.8/luarocks/share/lua/5.4/?/init.lua;/Users/tazz/.local/share/mise/downloads/lua/5.4.8/lua-5.4.8/luarocks-3.12.2/src/?.lua;/usr/local/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?/init.lua;/usr/local/lib/lua/5.4/?.lua;/usr/local/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua;/Users/tazz/.luarocks/share/lua/5.4/?.lua;/Users/tazz/.luarocks/share/lua/5.4/?/init.lua',
+}
 package.path = package.path .. ';' .. (table.concat(runtime_paths, ';'))
 print('package.path (after): ' .. package.path)
 
