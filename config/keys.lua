3e7097862718d4e75ca9851b7749e70045411491
tazz <tazz@tazz.cloud>
2025-12-31 12:38:40 -0500

    fix(keys): 🐛 hopefully fix scrollback key

--- config/keys.lua
+++ config/keys.lua
@@ -1,12 +1,11 @@
-local mods = require('config.mods')
 local wez = require('wezterm')
 
 wez.on('edit-scrollback', function(window, pane)
   local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
   local name = os.tmpname()
   local f = io.open(name, 'w+')
-  if not f then
-    print('failed to open scrollback: ' .. name)
+  if f == nil then
+    print('failed to open scrollback from: ' .. name)
     return
   end
 
