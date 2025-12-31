4d3d1f0fad7996b4cf168945f04fbeb58a0792f3
tazz <tazz@tazz.cloud>
2025-12-31 12:51:47 -0500

    revert(wezterm.lua): ⏪ remove debug line

--- wezterm.lua
+++ wezterm.lua
@@ -12,6 +12,5 @@ package.path = package.path .. ';' .. (table.concat(runtime_paths, ';'))
 print('package.path (after): ' .. package.path)
 
 local config = require('config')
-print('config: ' .. config)
 require('plugins').apply_to_config(config)
 return config
